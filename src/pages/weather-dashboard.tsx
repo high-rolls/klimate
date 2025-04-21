ba3213d87904fb8f4e20c1132a5ddf3526a03996
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2025-04-21 17:44:29 -0300

    add hourly temperature chart

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,4 +1,5 @@
 import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
@@ -106,12 +107,12 @@ const WeatherDashboard = () => {
       </div>
 
       <div className="grid gap-6">
-        <div>
+        <div className="flex flex-col lg:flex-row gap-4">
           <CurrentWeather
             data={weatherQuery.data}
             locationName={locationName}
           />
-          {/* hourly temperature */}
+          <HourlyTemperature data={forecastQuery.data} />
         </div>
         <div>
           {/* details */}
