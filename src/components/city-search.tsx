60e9839859e17b3af4ea6738e21037000811509e
Paulo Vitor F. G. Pivotto <paulo_vitor652@hotmail.com>
2025-04-30 13:13:25 -0300

    Fix color theme toggle being off screen on small width screens

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -45,7 +45,7 @@ const CitySearch = () => {
     <>
       <Button
         variant="outline"
-        className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        className="relative justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
         onClick={() => setOpen(true)}
       >
         <Search className="mr-2 h-4 w-4" />
